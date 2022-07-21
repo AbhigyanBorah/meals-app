3daa24234958fcd4ec2b11d85484ce5b9e1757d4
Abhigyan <abhigyanritiz63@gmail.com>
2022-07-21 15:16:21 +0530

    project start

---
+++ components/CategoryGridTile.js
@@ -0,0 +1,40 @@
+import {Pressable, View, Text, StyleSheet} from "react-native";
+
+function CategoryGridTile({title, color}) {
+
+    return (
+        <View style={[styles.gridItem]}>
+            <Pressable style={styles.button}>
+                <View style={styles.innerContainer}>
+                    <Text>{title}</Text>
+                </View>
+            </Pressable>
+        </View>
+    );
+}
+
+export default CategoryGridTile;
+
+const styles = StyleSheet.create({
+    gridItem: {
+        flex: 1,
+        margin: 16,
+        height: 150,
+        borderRadius: 15,
+        elevation: 6,
+        backgroundColor: 'white',
+        shadowColor: 'black',
+        shadowOffset: {width: 0, height: 1},
+        shadowOpacity: 0.15,
+        shadowRadius: '8',
+    },
+    button: {
+        flex: 1,
+    },
+    innerContainer: {
+        flex: 1,
+        padding: 16,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+});
